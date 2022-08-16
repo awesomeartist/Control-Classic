66f3b3f1afd66b980e78a7ec577d41e76099edeb
awesomeartist <liangxinmind@outlook.com>
2022-08-16 21:54:32 +0800

    rename

--- my_step.m
+++ my_step.m
@@ -1,3 +1,28 @@
+% system
 num = [0, 0, 50];
 den = [25, 2, 1];
+
+% response of step signal
+subplot(2, 2, 1);
 step(num, den);
+grid;
+
+% response of impulse signal
+subplot(2, 2, 2);
+impulse(num, den);
+grid;
+
+% response of any imput signal
+t = 0:0.01:150;
+u = sin(t);
+subplot(2, 2, 3);
+lsim(num, den, u, t);
+grid;
+
+% response of slope signal
+num1 = [0, 0, 0, 50];
+den1 = [25, 2, 1, 0];
+t = 0:0.01:100;
+subplot(2, 2, 4);
+step(num1, den1, t);
+grid;
--- variable_zp.m
+++ variable_zp.m
@@ -1,27 +1,31 @@
+%相同静态增益的不同二阶系统的单位阶跃响应图像
+
+% a环节
 a = 'p1 = 1 + j2,p2 = 1 - j2';
-num1 = [0, 0, 5];   %a环节
+num1 = [0, 0, 5];   
 den1 = [1, 2. 5];
 step(num1, den1);
 hold on
 
+% b环节
 b = 'p1 = 1 + j1,p2 = 1 - j1';
-num2 = [0, 0, 2];   %b环节
+num2 = [0, 0, 2];   
 den2 = [1, 2. 2];
 step(num2, den2);
-
 hold on
 
+% c环节
 c = 'p1 = 2 + j2,p2 = 2 - j2';
-num3 = [0, 0, 8];   %c环节
+num3 = [0, 0, 8];   
 den3 = [1, 4. 8];
 step(num3, den3);
-
 hold on 
 
+%d环节
 d = 'p1 = 2 + j1,p2 = 2 - j1';
-num4 = [0, 0, 5];   %d环节
+num4 = [0, 0, 5];   
 den4 = [1, 4. 5];
 step(num4, den4);
 
-
+% 标注
 legend(a, b, c, d)
